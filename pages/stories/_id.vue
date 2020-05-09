5b0060ca2882481e66f672abb187054e601acd33
nelermont <nelermont@yandex.ru>
2020-05-09 13:49:59 +0300

    добавил верстку своих страниц без адаптива

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <Special></Special>
+  </div>
 </template>
 
 <script>
-export default {};
+import Special from '@/components/Special';
+export default {
+  components: {
+    Special: Special,
+  },
+};
 </script>
 
 <style scoped></style>
