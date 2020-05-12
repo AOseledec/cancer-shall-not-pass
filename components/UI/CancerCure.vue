6d01e8c12abd23148ce6fd8967dbd12a7f552c8d
VladLo939 <vlo194@yandex.ru>
2020-05-13 02:50:28 +0300

    исправил header и  footer, сделал адаптив для footer, добавил все UI в components

---
+++ components/UI/CancerCure.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cure-component">
+    <a class="cure-component__link">Рак Лечится</a>
+    <a class="cure-component__link">Фонд Хабенского</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.cure-component {
+  width: 295px;
+  height: 134px;
+  background-color: #613a93;
+  display: flex;
+  flex-direction: column;
+}
+
+.cure-component__link {
+  margin-right: 101px;
+  text-decoration: none;
+  color: #c9c9c9;
+}
+
+.cure-component__link:hover {
+  color: white;
+}
+</style>
---
+++ components/UI/ShareButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <a href="" class="share-link">Поделитесь &#8599;</a>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.share-link {
+  margin-bottom: 44px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 24px;
+  color: #121212;
+  text-decoration: none;
+}
+
+.share-link:hover {
+  opacity: 0.8;
+}
+
+@media screen and (max-width: 1280px) {
+  .share-link {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 590px) {
+  .share-link {
+    font-size: 13px;
+    margin-bottom: 0;
+  }
+}
+</style>
